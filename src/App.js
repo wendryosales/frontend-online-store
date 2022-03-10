6fac5384a850eabe1dc959d019132fc78634bbb2
Wendryo S <wendryo.sales@gmail.com>
2022-03-10 16:29:54 -0300

    Co-authored-by: Caio Costa <cahito@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Link to="/"/>
+      </div>
+    );
+  }
 }
 
 export default App;
