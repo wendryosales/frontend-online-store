dd2d938a9a0fa6953d16b6810af26a448deffbbe
Bryan Fernandes <bryan.efernandes@gmail.com>
2022-03-11 15:52:06 -0300

    merge requisito 3 - componente categories

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -16,12 +16,6 @@ class Categories extends Component {
   fetchCategories = async () => {
     const data = await getCategories();
 
-    // Daria para fazer um map dentro desta função do próprio data
-
-    // Acho que seria ideal dar um bind na função, pois ela está executando automaticamente por ser arrow function
-
-    // Assim não seria necessário controlar state
-
     this.setState({
       data,
     });
